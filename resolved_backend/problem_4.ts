e68196cf180c99a0dfbaf32f4c71933933ffacbf
nipponwfm <nipponwfm@gmail.com>
2023-11-06 09:27:38 +0700

    [feat] resolved problem 4

--- resolved_backend/problem_4.ts
+++ resolved_backend/problem_4.ts
@@ -1,19 +1,24 @@
 function sum_to_n_a(n: number): number {
-  // your code here
+  let sum = 0;
+  for (let i = 1; i <= n; i++) sum += i;
 
-  return n;
+  return sum;
 }
 
 function sum_to_n_b(n: number): number {
-  // your code here
-
-  return 0;
+  return (n * (1 + n)) / 2;
 }
 
 function sum_to_n_c(n: number): number {
-  // your code here
+  function recursion(value = 0, index = 1) {
+    if (index > n) return value;
+    else return recursion(value + index, index + 1);
+  }
 
-  return 0;
+  return recursion();
 }
 
-console.log(sum_to_n_a(4));
+let test = 100;
+console.log(sum_to_n_a(test));
+console.log(sum_to_n_b(test));
+console.log(sum_to_n_c(test));
