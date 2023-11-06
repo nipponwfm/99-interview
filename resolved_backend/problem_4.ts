fd558f69b81debbfe7234e4f8c49c5da837934d0
nipponwfm <nipponwfm@gmail.com>
2023-11-06 09:14:32 +0700

    set up base

---
+++ resolved_backend/problem_4.ts
@@ -0,0 +1,19 @@
+function sum_to_n_a(n: number): number {
+  // your code here
+
+  return n;
+}
+
+function sum_to_n_b(n: number): number {
+  // your code here
+
+  return 0;
+}
+
+function sum_to_n_c(n: number): number {
+  // your code here
+
+  return 0;
+}
+
+console.log(sum_to_n_a(4));
